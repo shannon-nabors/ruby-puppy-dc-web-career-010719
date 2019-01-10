880112024d738d29e313c70d228e0a0d4af2b2de
shannon-nabors <shannon.nabors@gmail.com>
2019-01-10 16:27:37 -0500

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    puts @@all.map { |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+end
